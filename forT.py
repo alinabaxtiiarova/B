ba5ccde1e542e9e20b16e13acc21b105fab84018
alinabaxtiiarova <50831067+alinabaxtiiarova@users.noreply.github.com>
2020-04-07 00:21:01 +0300

    Create forT.py

---
+++ forT.py
@@ -0,0 +1,10 @@
+import telebot
+bot = telebot.TeleBot('1141594126:AAGOHEhs7hFHMEUwJlYZOsjeDMf73JscbuY')
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): 
+    bot.send_message(message.chat.id, message.text)
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+
+bot.polling(none_stop = True, interval = 0)
