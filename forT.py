639f545331c7296e2616491eebed0b0f65128667
alinabaxtiiarova <50831067+alinabaxtiiarova@users.noreply.github.com>
2020-04-14 22:45:35 +0300

    Update forT.py

--- forT.py
+++ forT.py
@@ -1,5 +1,18 @@
 import telebot
+from telebot import types
 bot = telebot.TeleBot('1141594126:AAGOHEhs7hFHMEUwJlYZOsjeDMf73JscbuY')
+
+@bot.message_handler(commands=['trello'])
+def open_website(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Перейти", url="https://trello.com"))
+    bot.send_message(message.chat.id, "Не забудь заполнить", parse_mode='html', reply_markup=markup)
+@bot.message_handler(commands=['yclients'])
+def open_website(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Перейти", url="https://yclients.com"))
+    bot.send_message(message.chat.id, "Расписание", parse_mode='html', reply_markup=markup)
+
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): 
