2e1067c2045bbcfa30030ed94da827c5909ff6c4
alinabaxtiiarova <50831067+alinabaxtiiarova@users.noreply.github.com>
2020-06-16 22:14:08 +0300

    Update forT.py

--- forT.py
+++ forT.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 bot = telebot.TeleBot('1141594126:AAGOHEhs7hFHMEUwJlYZOsjeDMf73JscbuY')
-
+import keyboards as kb
 @bot.message_handler(commands=['trello'])
 def open_website(message):
     markup = types.InlineKeyboardMarkup()
@@ -12,13 +12,17 @@ def open_website(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton("Перейти", url="https://yclients.com"))
     bot.send_message(message.chat.id, "Расписание", parse_mode='html', reply_markup=markup)
-
-
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): 
     bot.send_message(message.chat.id, message.text)
     if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+        bot.send_message("Привет, чем я могу тебе помочь?")
     else :
         bot.send_message(message.from_user.id, "Доброй ночи!")
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply('Ghbdt', reply_markup=kb.greet_kb
+
 bot.polling(none_stop = True, interval = 0)
