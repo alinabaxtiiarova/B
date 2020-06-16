e9c302bfed8228b175d7391365d29dbf57cf5c74
alinabaxtiiarova <50831067+alinabaxtiiarova@users.noreply.github.com>
2020-06-16 22:20:16 +0300

    Update forT.py

--- forT.py
+++ forT.py
@@ -20,9 +20,4 @@ def repeat_all_messages(message):
     else :
         bot.send_message(message.from_user.id, "Доброй ночи!")
 
-
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply('Ghbdt', reply_markup=kb.greet_kb
-
 bot.polling(none_stop = True, interval = 0)
