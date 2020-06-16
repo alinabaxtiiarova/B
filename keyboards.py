fe0bf7ef990baac8b5096d728b7a9dec3639a3df
alinabaxtiiarova <50831067+alinabaxtiiarova@users.noreply.github.com>
2020-06-16 20:15:02 +0300

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,5 @@
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+button_hi1 = KeyboardButton('shshhsh')
+
+greet_kb = ReplyKeyboardMarkup()
+greet_kb.add(button_hi)
